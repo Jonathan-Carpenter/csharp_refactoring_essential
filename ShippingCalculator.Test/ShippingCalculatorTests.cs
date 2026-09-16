91c84368f0f0761d63c338ceacc84dcf25ec2b01
Lili Covileac <Lili.Covileac@mindlinksoft.com>
2026-09-16 12:16:38 +0100

    It works!

---
+++ ShippingCalculator.Test/ShippingCalculatorTests.cs
@@ -0,0 +1,16 @@
+﻿namespace ShippingCalculator.Test
+{
+    public class ShippingCalculatorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
